0c0f49375fd7227c187fd4f08fbac9b0377c3e3a
haeun9969 <haeun9969@naver.com>
2022-01-12 09:09:49 +0900

    make it better

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+var router = require('./router/main')(app);
+
+//app setting
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static('public'));
+
+//server setting
+var server = app.listen(3000, function(){
+  console.log("Express server has started on port 3000")
+  app.use(express.static('public'));
+});
+
+
+
+
---
+++ router/main.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+module.exports = function(app){
+    app.get('/',function(req,res){
+       res.render('index.html')
+    });
+}
